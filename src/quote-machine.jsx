e1f293f7b380d1f9a8fbeeebf04b12af22e3bf5c
ECMGS <42187626+ECMGS@users.noreply.github.com>
2019-08-09 22:42:22 +0200

    first update

---
+++ src/quote-machine.jsx
@@ -0,0 +1,16 @@
+
+class QuoteBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id="quote-box">
+                
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<QuoteBox />, document.getElementById("quote-app"))
